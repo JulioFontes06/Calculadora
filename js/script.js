44fd63b1e506c9d1f4e7b3dd6b8070d0771076ea
JulioFontes06 <julio.developer06@gmail.com>
2024-01-26 18:16:28 -0300

    Incremento da opção de digitação pelo teclado

--- js/script.js
+++ js/script.js
@@ -15,7 +15,31 @@ lastNumberDelet.addEventListener('click', () => {
 })
 
 let caracterOperation = ['/', '*', '-', '.', '+']
+let alowedkey = ['/', '*', '-', '+', '.', '0', '1','2','3','4','5','6','7','8','9']
 
+addEventListener('keydown', (ev) => {
+    if(alowedkey.includes(ev.key)){
+        const currentInput = input.innerText
+        const selectedKey = ev.key
+        
+        let isOperationKey = caracterOperation.includes(currentInput.slice(-1))
+
+        if(caracterOperation.includes(selectedKey) && isOperationKey){
+            input.innerText = currentInput.slice(0, -1) + ev.key
+            return
+        }
+
+        input.innerText += ev.key
+    }
+
+    if(ev.key === 'Enter'){
+        calculate()
+    }
+
+    if(ev.key === 'Backspace'){
+        input.innerText = input.innerText.slice(0, -1)
+    }
+})
 
 keyInput.forEach((key) => {
     key.addEventListener('click', () => {
