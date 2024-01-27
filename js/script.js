62659d10fd22a932b70ddd5cc0c841e763dc1041
JulioFontes06 <julio.developer06@gmail.com>
2024-01-27 09:29:18 -0300

    Criação da alteração de temas

--- js/script.js
+++ js/script.js
@@ -5,6 +5,31 @@ const lastNumberDelet = document.getElementById('lastRemove')
 const clearInput = document.getElementById('remove')
 const calc = document.getElementById('equal')
 
+const changeTheme = document.getElementById('theme-change')
+
+const keyTheme = document.querySelectorAll('.key')
+const mainTheme = document.querySelector('main')
+
+changeTheme.addEventListener('click', () => {
+    const body = document.querySelector('body')
+        changeTheme.classList.toggle('dark-theme-change-button')
+        changeTheme.classList.toggle('light-theme-change-button')
+    
+        body.classList.toggle('dark-theme-body')
+        body.classList.toggle('light-theme-body')
+
+        mainTheme.classList.toggle('dark-theme-main')
+        mainTheme.classList.toggle('light-theme-main')
+
+        input.classList.toggle('dark-theme-input')
+        input.classList.toggle('light-theme-input')
+
+    keyTheme.forEach((key) => {
+        key.classList.toggle('light-theme-button')
+        key.classList.toggle('dark-theme-button')
+    })
+    
+})
 
 clearInput.addEventListener('click', () => {
     input.innerText = ''
